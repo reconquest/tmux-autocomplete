be6ca77b6a149ef934c2ddb9642c96fb3e3fc1d4
Egor Kovetskiy <e.kovetskiy@gmail.com>
2018-08-22 15:03:13 +0300

    rework mkfifo

--- fifo.go
+++ fifo.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"syscall"
 	"time"
 )
 
@@ -14,15 +13,15 @@ func mkfifo() (*os.File, string, error) {
 		fmt.Sprintf("tmux-autocomplete_%d", time.Now().UnixNano()),
 	)
 
-	fmt.Fprintf(os.Stderr, "XXXXXX fifo.go:16 syscall mkfifo\n")
-	err := syscall.Mkfifo(name, 0666)
-	if err != nil {
-		return nil, "", err
-	}
+	//fmt.Fprintf(os.Stderr, "XXXXXX fifo.go:16 syscall mkfifo\n")
+	//err := syscall.Mkfifo(name, 0666)
+	//if err != nil {
+	//return nil, "", err
+	//}
 
 	fmt.Fprintf(os.Stderr, "XXXXXX fifo.go:22 openfile mode\n")
 
-	file, err := os.OpenFile(name, os.O_RDONLY, os.ModeNamedPipe)
+	file, err := os.OpenFile(name, os.O_CREATE|os.O_RDONLY, os.ModeNamedPipe)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "XXXXXX fifo.go:26 err: %s\n", err)
 		return nil, "", err
